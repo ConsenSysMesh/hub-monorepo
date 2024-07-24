147f8b44eb8220cee353d4dd7c122b88d23bf059
Vlad Kutsenko <vladdie@gmail.com>
2024-07-24 14:32:03 -0400

    fetching both owned and worn rings on page load for the active FID

--- apps/rings-next/api-client.ts
+++ apps/rings-next/api-client.ts
@@ -14,7 +14,7 @@ import {
   makeRelationshipRemove,
 } from '@farcaster/hub-web';
 import { User, RelationshipTypes } from '@farcaster/rings-next/types.d';
-import { convertHexHash } from '@farcaster/rings-next/state/utils';
+import { convertHexHash, getObjectRefStoreId, getMessageStoreId } from '@farcaster/rings-next/state/utils';
 
 const API_HOST = process.env.NEXT_PUBLIC_API_URL;
 const API_URL = `${API_HOST}/v1`;
@@ -50,7 +50,19 @@ const getApiClient = () => {
     return result.data as MessagesResponse;
   };
 
-  const getRingWearerRels = async (ringRef: ObjectRef) => {
+  const getRingWearerRels = async (fid: number) => {
+    const result = await client.get('/relationshipsByRelatedObjectRef', {
+      params: {
+        ref_type: ObjectRefTypes.FID,
+        object_ref_fid: fid,
+        ref_direction: RefDirection.TARGET,
+        type: RelationshipTypes.Wearer,
+      }
+    });
+    return result.data as MessagesResponse;
+  };
+
+  const getRingWearerRelsByRing = async (ringRef: ObjectRef) => {
     const result = await client.get('/relationshipsByRelatedObjectRef', {
       params: {
         ref_type: ObjectRefTypes.OBJECT,
@@ -59,6 +71,20 @@ const getApiClient = () => {
         object_ref_hash: ringRef.hash,
         ref_direction: RefDirection.SOURCE,
         type: RelationshipTypes.Wearer,
+      }
+    });
+    return result.data as MessagesResponse;
+  }
+
+  const getRingOwnerRelsByRing = async (ringRef: ObjectRef) => {
+    const result = await client.get('/relationshipsByRelatedObjectRef', {
+      params: {
+        ref_type: ObjectRefTypes.OBJECT,
+        object_ref_network: NETWORK,
+        object_ref_fid: ringRef.fid,
+        object_ref_hash: ringRef.hash,
+        ref_direction: RefDirection.SOURCE,
+        type: RelationshipTypes.Owner,
       }
     });
     return result.data as MessagesResponse;
@@ -86,8 +112,7 @@ const getApiClient = () => {
       // TODO: shape the data as a single user object before passing it down
       return result.data as MessagesResponse;
     },
-    getOwnedRings: async (fid: number) => {
-      // first find the owner relationships for the user's fid
+    getOwnedAndWornRings: async (fid: number) => {
       const results = {
         rings: [] as Array<Message>,
         stones: [] as Array<Message>,
@@ -95,16 +120,51 @@ const getApiClient = () => {
         users: [] as Array<User>,
       }
       const fids: Array<number> = [];
+      let ownedRingRefs: Array<ObjectRef> = [];
+      let wornRingRefs: Array<ObjectRef> = [];
+      let ownerRels: Array<Message> = [];
+      let wearerRels: Array<Message> = [];
+
+      // Get the owner Relationships for the FID
       const ownerResult = await getRingOwnerRels(fid);
-      const ownerRels = ownerResult.messages;
+      ownerRels = ownerResult.messages;
       results.relationships.push(...ownerRels);
       for (let ownerRel of ownerRels) {
-        console.log('Found a ring owner relationship!', ownerRel);
-        if (!ownerRel.data?.relationshipAddBody) continue; // null checks to make TS happy
-        let ringRef = ownerRel.data.relationshipAddBody.source as ObjectRef;
-        fids.push(ownerRel.data.relationshipAddBody.target?.fid as number);
-      
+        // console.log('Found a ring owner relationship!', ownerRel);
+        let ringRef = ownerRel.data?.relationshipAddBody?.source as ObjectRef;
+        ownedRingRefs.push(ringRef);
+      }
+
+      // Get the wearer Relationships for the FID
+      const wearerResult = await getRingWearerRels(fid);
+      wearerRels = wearerResult.messages;
+      results.relationships.push(...wearerRels);
+      for (let wearerRel of wearerRels) {
+        // console.log('Found a ring wearer relationship!', wearerRel);
+        let ringRef = wearerRel.data?.relationshipAddBody?.source as ObjectRef;
+        wornRingRefs.push(ringRef);      
+      }
+
+      // For every owned ring, find the ring wearers
+      for (let ownedRingRef of ownedRingRefs) {
+        const ownedRingWearerResult = await getRingWearerRelsByRing(ownedRingRef);
+        const ownedRingWearerRels = ownedRingWearerResult.messages;
+        wearerRels.push(...ownedRingWearerRels);
+      }
+
+      // For every worn ring, find the ring owners
+      for (let wornRingRef of wornRingRefs) {
+        const wornRingsOwnerResult = await getRingOwnerRelsByRing(wornRingRef);
+        const wornRingsOwnerRels = wornRingsOwnerResult.messages;
+        ownerRels.push(...wornRingsOwnerRels);
+      }
+
+      const uniqueRingRefs = _.uniqBy([...ownedRingRefs, ...wornRingRefs], ref => getObjectRefStoreId(ref));
+
+      // Having found all relevant ring refs, fetch the ring objects with stone tags
+      for (let ringRef of uniqueRingRefs) {
         // get the ring object
+        // console.log('Ring hash bytes', hexStringToBytes(ringRef.hash));
         let ringObjResult = await getRingObject(ringRef);
         if (!ringObjResult.object?.data) {
           continue;
@@ -114,25 +174,19 @@ const getApiClient = () => {
         let ringTags = ringObjResult.tags;
         results.stones.push(...ringTags);
         console.log('Found a ring!', ringObj, ringTags);
-    
-        // console.log('Ring hash bytes', hexStringToBytes(ringRef.hash));
-        
-        // now also get the ring wearers to have complete ring state?
-        const wearerResult = await getRingWearerRels(ringRef);
-        const wearerRels = wearerResult.messages;
-        results.relationships.push(...wearerRels);
-        for (let wearerRel of wearerRels) {
-          console.log('Found a ring wearer relationship!', wearerRel);
-          if (!wearerRel.data?.relationshipAddBody) continue; // null checks to make TS happy
-          fids.push(wearerRel.data.relationshipAddBody.target?.fid as number);
-          
-          // TODO: figure out whether we're loading in the profiles of all the users involved with the rings
-          
-        }
       }
-      results.users = _.uniq(fids).map((fid: number) => ({ fid } as User)); // lean User objects without profile info for now
+
+      // Dedupe owner and wearer relationships in case some FIDs are participating as both owners and wearers of rings
+      results.relationships = _.uniqBy([...ownerRels, ...wearerRels], rel => getMessageStoreId(rel));
+
+      // TODO: figure out whether we're loading in the profiles of all the users involved with the rings
+      results.users =
+        // all FIDs involved are the target of the relationships discovered
+        _.uniq(results.relationships.map(r => r.data?.relationshipAddBody?.target?.fid as number))
+          .map((fid: number) => ({ fid } as User)); // lean User objects without profile info for now
       return results;
     },
+    
     updateStone: async (fid: number, changes: TagBody) => {
       const signer = SignersByFid[fid];
       if (!signer) {
